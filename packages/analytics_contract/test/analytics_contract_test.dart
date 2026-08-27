1f56087697c797cb4685ad4ac704774f5775df64
xwiz <c.opata@robosyslive.com>
2026-08-27 21:59:01 +0100

    test(events): assert stable event identity

--- packages/analytics_contract/test/analytics_contract_test.dart
+++ packages/analytics_contract/test/analytics_contract_test.dart
@@ -4,12 +4,14 @@ import 'package:test/test.dart';
 void main() {
   test('serializes canonical event envelope', () {
     final event = MosaicEventEnvelope(
+      eventId: 'evt_1',
       event: MosaicEventName.playStarted,
       occurredAt: DateTime.utc(2026, 8, 27, 18),
       actorId: 'actor_1',
       sessionId: 'session_1',
       playRevisionId: 'rev_1',
     );
+    expect(event.toJson()['eventId'], 'evt_1');
     expect(event.toJson()['version'], 1);
     expect(event.toJson()['playRevisionId'], 'rev_1');
   });
