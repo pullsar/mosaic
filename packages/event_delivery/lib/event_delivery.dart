b152fdaac741f5b7b8b6092ee0ced5f57339c36c
xwiz <c.opata@robosyslive.com>
2026-08-28 21:41:06 +0100

    style(events): order public exports

--- packages/event_delivery/lib/event_delivery.dart
+++ packages/event_delivery/lib/event_delivery.dart
@@ -2,7 +2,7 @@ library;
 
 export 'src/event_delivery_core.dart';
 export 'src/http_event_transport.dart';
-export 'src/sqlite_event_outbox.dart'
-    if (dart.library.js_interop) 'src/sqlite_event_outbox_stub.dart';
 export 'src/indexed_db_event_store_stub.dart'
     if (dart.library.js_interop) 'src/indexed_db_event_store.dart';
+export 'src/sqlite_event_outbox.dart'
+    if (dart.library.js_interop) 'src/sqlite_event_outbox_stub.dart';
