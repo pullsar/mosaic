b4f15398347380b1aabaaeb6e39e335f3b96bb66
xwiz <c.opata@robosyslive.com>
2026-08-28 19:40:29 +0100

    feat(platform): export runtime diagnostics adapter

--- packages/platform_flutter/lib/platform_flutter.dart
+++ packages/platform_flutter/lib/platform_flutter.dart
@@ -3,3 +3,5 @@ library;
 export 'src/lifecycle.dart';
 export 'src/media_picker.dart';
 export 'src/permissions.dart';
+export 'src/runtime_browser_classifier.dart';
+export 'src/runtime_diagnostics.dart';
