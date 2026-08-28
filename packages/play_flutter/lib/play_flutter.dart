1bba4444b61da96548c9b1b003a62ccfde6c8591
xwiz <c.opata@robosyslive.com>
2026-08-28 03:32:44 +0100

    feat(renderer): export Play media owner identity

--- packages/play_flutter/lib/play_flutter.dart
+++ packages/play_flutter/lib/play_flutter.dart
@@ -4,6 +4,7 @@ export 'src/play_audio_asset_codec.dart';
 export 'src/play_audio_renderer.dart';
 export 'src/play_canvas_renderer.dart';
 export 'src/play_input_primitives.dart';
+export 'src/play_media_identity.dart';
 export 'src/play_media_layer_renderer.dart';
 export 'src/play_surface.dart';
 export 'src/play_video_asset_codec.dart';
