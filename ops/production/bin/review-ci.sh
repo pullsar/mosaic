57b3e45383b9f69d802fa6f7a96035ecb4b5b56d
Chibueze Opata <copata@venmail.io>
2026-08-29 10:16:54 +0100

    fix(ci): annotate trap-invoked review handlers

--- ops/production/bin/review-ci.sh
+++ ops/production/bin/review-ci.sh
@@ -13,11 +13,13 @@ cleanup_status=0
 
 builder_git() { runuser -u mixli-build -- git "$@"; }
 
+# shellcheck disable=SC2317 # Invoked indirectly by the TERM/INT trap.
 on_term() {
   conclusion=cancelled
   exit 143
 }
 
+# shellcheck disable=SC2317 # Invoked indirectly by the EXIT trap.
 cleanup() {
   local original="$1"
   trap - EXIT TERM INT
