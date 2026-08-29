fd6ccd87e819a7fa04130c4a155eea749debcaa4
xwiz <c.opata@robosyslive.com>
2026-08-29 20:49:06 +0100

    feat(m2): wire managed delivery into API process

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,6 +2,12 @@ import {Pool} from 'pg';
 import {buildApp} from './app.js';
 import {loadConfig} from './config.js';
 import {PostgresConsumerRepository} from './consumer_repository.js';
+import {MediaDeliveryService} from './media_delivery.js';
+import {loadMediaDeliveryStorageConfig} from './media_delivery_config.js';
+import {LocalMediaDeliveryObjectReader} from './media_delivery_local_storage.js';
+import {registerMediaDeliveryRoutes} from './media_delivery_routes.js';
+import {S3MediaDeliveryObjectReader} from './media_delivery_s3_storage.js';
+import {PostgresMediaPublicationGate} from './media_publication.js';
 import {PostgresRepository} from './repository.js';
 
 const config = loadConfig();
@@ -14,6 +20,27 @@ const app = buildApp({
   logLevel: config.logLevel,
   allowedWebOrigins: config.allowedWebOrigins,
 });
+
+const mediaDeliveryConfig = loadMediaDeliveryStorageConfig();
+if (mediaDeliveryConfig.storageMode !== 'disabled') {
+  const reader = mediaDeliveryConfig.storageMode === 'local'
+    ? new LocalMediaDeliveryObjectReader(mediaDeliveryConfig.objectRoot)
+    : new S3MediaDeliveryObjectReader({
+        endpoint: mediaDeliveryConfig.s3Endpoint,
+        bucket: mediaDeliveryConfig.s3Bucket,
+        region: mediaDeliveryConfig.s3Region,
+        accessKeyId: mediaDeliveryConfig.s3AccessKeyId,
+        secretAccessKey: mediaDeliveryConfig.s3SecretAccessKey,
+        ...(mediaDeliveryConfig.s3SessionToken === undefined
+          ? {}
+          : {sessionToken: mediaDeliveryConfig.s3SessionToken}),
+        requestTimeoutMs: mediaDeliveryConfig.storageTimeoutMs,
+      });
+  registerMediaDeliveryRoutes(
+    app,
+    new MediaDeliveryService(new PostgresMediaPublicationGate(pool), reader),
+  );
+}
 
 const shutdown = async (signal: string) => {
   app.log.info({signal}, 'shutting down');
