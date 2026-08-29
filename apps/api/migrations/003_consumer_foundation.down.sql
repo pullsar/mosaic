6b6cc11f10b20119260801eccc367bab535eb6fb
xwiz <c.opata@robosyslive.com>
2026-08-29 09:38:00 +0100

    feat(feed): make consumer schema reversible

---
+++ apps/api/migrations/003_consumer_foundation.down.sql
@@ -0,0 +1,5 @@
+drop table if exists feed_decision_items;
+drop table if exists feed_decisions;
+drop table if exists feed_catalog_entries;
+drop table if exists play_revision_topics;
+drop table if exists actor_topic_preferences;
